ac029f5486e49b2663d142e521b04fe4d3779211
lequocduyquang <duyquangbtx@gmail.com>
2022-05-21 16:40:57 +0700

    init broker service

---
+++ g-broker/cmd/api/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const port = "80"
+
+type Config struct{}
+
+func main() {
+	app := Config{}
+
+	log.Printf("Starting g-broker service on port %s\n", port)
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", port),
+		Handler: app.routes(),
+	}
+
+	// Start the server
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+
+}
