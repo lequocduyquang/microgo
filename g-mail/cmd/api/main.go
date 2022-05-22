67c3abb1ca33f0379e8a587ceb803faa69b012d5
lequocduyquang <duyquangbtx@gmail.com>
2022-05-22 10:03:04 +0700

    setup g-mail service

---
+++ g-mail/cmd/api/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Config struct{}
+
+const port = "80"
+
+func main() {
+	app := Config{}
+
+	log.Printf("Starting g-mail service on port %s\n", port)
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", port),
+		Handler: app.routes(),
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+
+}
